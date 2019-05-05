0d757b5e3bd8e6190ee949369632ce197cf2ea05
tloki <tomislav.lokotar@icloud.com>
2019-05-05 16:28:45 +0200

    added working container

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,3 +1,5 @@
 #/usr/bin/env bash
 cp ../run.sh ./run.sh
-docker build . -t "openblas-npy-build"
+docker build . -t "openblas-npy-build" 2>&1 | tee out2.tmp
+cat out2.tmp | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" > build.log
+rm out2.tmp
--- docker/run_image.sh
+++ docker/run_image.sh
@@ -1,3 +1,6 @@
 #/usr/bin/env bash
 
-docker run -it openblas-npy-build 2>&1 | tee out.log
+docker run -it openblas-npy-build 2>&1 | tee out1.tmp
+cat out1.tmp | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" > run.log
+rm out1.tmp
+
